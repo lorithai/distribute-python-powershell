133bf21cdeb0062ce49bff2c1dd74dca2a6fcdbf
lorithai <lorithai@gmail.com>
2023-11-14 12:41:15 +0100

    Update install_py_env.bat
    Added some comments clarifying how the prevent cmd from closing command works

--- install_py_env.bat
+++ install_py_env.bat
@@ -1,6 +1,10 @@
 @echo off
 
-REM don't close cmd on error
+REM don't close cmd on error. 
+REM Does this by checking if variable in_subprocess is defined. If not, create a new cmd (/k prevents it from closing) 
+REM ^& becomes & (and) while %0 is the path of the currently running bash script and %* are all input paramters. 
+REM Basically, it opens a new cmd and invokes the bash script again. 
+REM Finally exit the inital bash script. 
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
 REM "-ExecutionPolicy Bypass" tries to surpress standard powershell restriction
